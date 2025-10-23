ea7475d9c2b0288356bbfab2e802217e60f965a0
naeemsadozai <muhammadnaeemsadozai@gmail.com>
2025-10-23 16:23:58 +0500

    Update Navbar.jsx

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -23,7 +23,8 @@ const Navbar = () => {
   useEffect(()=>{
     let check = async()=>{
       try {
-        let response = await axios.get('https://cars-selling-website-backend.onrender.com/auth/isLoggedin',{withCredentials:true});
+        let response = await axios.get('https://cars-selling-website-backend.onrender.com/auth/isLoggedin',{},{withCredentials:true});
+        console.log(response.data.isLoggedin)
         if(response.data.isLoggedin){
           setIsLoggedin(true)
         }else{
@@ -150,3 +151,4 @@ const Navbar = () => {
 
 
 export default Navbar;
+
