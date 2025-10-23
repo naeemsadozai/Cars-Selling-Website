9e3e0c963f950222925697a0fc2cd691dc7f2d6b
naeemsadozai <muhammadnaeemsadozai@gmail.com>
2025-10-23 16:30:47 +0500

    Update isLoggedin.js

--- backend/Middlewares/isLoggedin.js
+++ backend/Middlewares/isLoggedin.js
@@ -2,11 +2,15 @@ import { decodeToken } from "../Utils/decodeToken.js"
 
 export const isLoggedin = async (req,res,next)=>{
     let cookie = process.env.COOKIE_NAME;
+    console.log(cookie);
     let token = req.cookies[cookie];
     if(!token){
+        console.log("NO TOKKEN");
         return res.status(200).json({message: "Not logged in",isLoggedin:false})
     }
     let user = decodeToken(token)
+    console.log(user);
     req.user = {...user}
     next()
+
 }
